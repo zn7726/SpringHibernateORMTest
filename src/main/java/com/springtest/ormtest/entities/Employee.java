68d28c593db8d1608ca4ac7c2807702df57cbe88
Nan <zn7726@hotmail.com>
2017-05-02 12:08:15 +1000

    map to collection of simple type

--- src/main/java/com/springtest/ormtest/entities/Employee.java
+++ src/main/java/com/springtest/ormtest/entities/Employee.java
@@ -1,11 +1,19 @@
 package com.springtest.ormtest.entities;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OrderColumn;
 import javax.persistence.Table;
 
 @Entity
@@ -22,7 +30,31 @@ public class Employee implements Serializable {
 	
 	@ManyToOne
 	private Employee supervisor;
-
+	
+	// columns in Address table will be added/embedded in Employee table
+	@Embedded
+	private Address address;
+	
+// mapping collection of simple type //////////////////
+	@ElementCollection	// only this annotation is mandatory
+	@CollectionTable(	// following 2 annotations are for customization only
+			name="employee_phones",
+			joinColumns=@JoinColumn(name="customer_id"))
+	@Column(name="phone")
+	private Set<String> phones;	// objects in a Set is unordered, similar to records in a table
+	
+	@ElementCollection	
+	@CollectionTable(	
+			name="employee_phones_ordered",
+			joinColumns=@JoinColumn(name="customer_id"))
+	@OrderColumn(name="phone_order")
+	@Column(name="phone")
+	private List<String> orderedPhones;	// objects in a List is ordered, 
+										// need to use either @OrderBy or an extra column @OrderColumn
+	
+	
+// mapping collection of Object type //////////////////	
+	
 	public Employee()  {	}
 
 	public int getId() {
@@ -52,4 +84,29 @@ public class Employee implements Serializable {
 		return this;
 	}
 
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Set<String> getPhones() {
+		return phones;
+	}
+
+	public void setPhones(Set<String> phones) {
+		this.phones = phones;
+	}
+
+	public List<String> getOrderedPhones() {
+		return orderedPhones;
+	}
+
+	public void setOrderedPhones(List<String> orderedPhones) {
+		this.orderedPhones = orderedPhones;
+	}
+	
+	
 }
