9ade08ea29d6a5586ddc44997999691f2451bd47
ezhang <ezhang@RI-6001.rawideas.local>
2017-05-22 10:28:27 +1000

    to add scheduler and tasks in the proj

--- src/main/java/com/springtest/SpringHibernateOrmApplication.java
+++ src/main/java/com/springtest/SpringHibernateOrmApplication.java
@@ -2,8 +2,10 @@ package com.springtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringHibernateOrmApplication {
 	
 	public static void main(String[] args) {
